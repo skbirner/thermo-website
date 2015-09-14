34adceb6292d7b8b40ecaef23b74dfa907bddad3
skbirner <skbirner@stanford.edu>
2015-09-14 15:26:03 -0700

    initial
    First pass at website, with Gibbs curve and freezing point depression
    example.

---
+++ pdControl_copy.js
@@ -0,0 +1,219 @@
+/**
+ * Created by secretariat21 on 9/3/15.
+ */
+//This page is for variable and function definitions
+
+/*
+ Returns the input clipped my min and max values
+ */
+function bound(value, min, max) {
+    return Math.max(Math.min(value, max), min);
+}
+
+var panelPtMass = {
+    width: parseInt(document.getElementById("panelPtMass").style.width),
+    height: parseInt(document.getElementById("panelPtMass").style.height),
+    svg: d3.select("#panelPtMass"),
+};
+
+var formatLabelString = d3.format(".2f");
+
+/*
+ Define a slider class
+ */
+function Slider(domain, range, sliderName, circleName, labelName, initialValue) {
+    this.x = 0;
+    this.y = document.getElementById(sliderName).getAttribute("y2");
+    this.value = initialValue ? initialValue : 0;
+    this.needsRedraw = true;
+
+    this.xRange = domain;
+    this.valueRange = range;
+
+    this.scaleXToValue = d3.scale.linear()
+        .domain(this.xRange)
+        .range(this.valueRange);
+    this.scaleValueToX = d3.scale.linear()
+        .domain(this.valueRange)
+        .range(this.xRange);
+
+
+    this.getValue = function() {
+        return this.value
+    };
+    this.setValue = function(value) {
+        this.setX(this.scaleValueToX(value));
+    }
+    this.setX = function(x) {
+        this.x = bound(x, this.xRange[0], this.xRange[1]);
+        this.value = this.scaleXToValue(this.x);
+        this.needsRedraw = true;
+    }
+    this.redraw = function() {
+        if (!this.needsRedraw) {
+            return;
+        }
+        this.needsRedraw = false;
+        d3.select(circleName)
+            .attr("cx", this.x)
+            .attr("cy", this.y);
+        d3.select(labelName)
+            .text(formatLabelString(this.value))
+            .attr("x", this.x);
+        this.onRedraw();
+    }
+
+    this.onRedraw = function() {}
+
+    this.initialize = function() {
+        this.setValue(this.value);
+        var sliderReference = this;
+
+        d3.select(circleName).call(
+            d3.behavior.drag()
+                .on("drag", function() {
+                    sliderReference.setX(d3.event.x);
+                }));
+
+        d3.select("#" + sliderName).on("click", function() {
+            var clickPos = d3.mouse(this);
+            sliderReference.setX(clickPos[0]);
+        })
+    }
+
+    this.initialize();
+}
+
+var sliderDamp = new Slider([160, 300], [0, 0.5], "dampingRail", "#dampingCircle", "#dampingLabel");
+
+var stepResponse = {
+    tLow: 0,
+    tUpp: 1.0,
+    data: new Array(100),
+
+    m: 0.1,
+    b: 2.0,
+    R: 8.3145,
+    T: 273,
+
+    xScale: d3.scale.linear()
+        .domain([0, 1.0])       //range for x values
+        .range([450, 700]),     //location of x axis on screen (pixels)
+
+    yScale: d3.scale.linear()
+        .domain([-1550, 0])  //range for y values
+        .range([150, 30]),      //location of y axis on screen (pixels)
+
+    myDataCurve: function(t) {
+            m = this.R*this.T*(Math.log(this.x) - Math.log(1-this.x));
+            b = this.R*this.T*(Math.log(1-this.x));
+
+            var x = m*t + b;
+
+            return this.yScale(x);
+    },
+
+    buildData: function() {
+        var nData = this.data.length;
+        var shift = this.tLow;
+        var slope = (this.tUpp - this.tLow) / (nData - 1);
+        for (var i = 0; i < nData; i++) {
+            var t = shift + i * slope;
+            this.data[i] = [this.xScale(t), this.myDataCurve(t)];
+        }
+
+    },
+    redraw: function() {
+        this.x = sliderDamp.getValue();
+        this.buildData();
+        d3.select("#panelTwo").select(".curve")
+            .attr("d", smoothCurve(this.data));
+    }
+}
+
+//Initialize the step response curve
+d3.select("#panelTwo").append("path")
+    .attr("class", "curve");
+
+stepResponse.buildData();
+
+
+var GT_response = {
+    tLow: 0.4,
+    tUpp: 1,
+    data: new Array(100),
+    data2: new Array(100),
+
+    R: 8.3145,
+    T: 273,
+    S: 80,
+    b: 10,
+
+    xScale: d3.scale.linear()
+        .domain([0.4, 1])       //range for x values
+        .range([300, 575]),     //location of x axis on screen (pixels)
+
+    yScale: d3.scale.linear()
+        .domain([-10, 30])  //range for y values
+        .range([50, -50]),      //location of y axis on screen (pixels)
+
+    myDataCurve: function(t) {
+        Gibbs_o = -this.S*t+this.b;
+        Gibbs_mix = this.R*t*Math.log(1-this.x);
+
+        var x = Gibbs_o + Gibbs_mix;
+
+        return this.yScale(x);
+    },
+
+    //X_Intercept: this.myDataCurve(0.6),
+
+
+    buildData: function() {
+        var nData = this.data.length;
+        var shift = this.tLow;
+        var slope = (this.tUpp - this.tLow) / (nData - 1);
+        for (var i = 0; i < nData; i++) {
+            var t = shift + i * slope;
+            this.data[i] = [this.xScale(t), this.myDataCurve(t)];
+         //   this.data2[i] = [0.6, this.yScale(t)];
+        }
+    },
+
+    redraw: function() {
+        this.x = sliderDamp.getValue();
+        this.buildData();
+        d3.select("#panelPtMass").select(".curve")
+            .attr("d", smoothCurve(this.data));
+        //d3.select("#panelPtMass").select(".curve")
+        //    .attr("d", smoothCurve(this.data2));
+    }
+}
+
+var smoothCurve = d3.svg.line()
+    .x(function(d) {
+        return d[0];
+    })
+    .y(function(d) {
+        return d[1];
+    })
+    .interpolate("monotone");
+
+//Initialize the GT curve
+d3.select("#panelPtMass").append("path")
+    .attr("class", "curve");
+
+GT_response.buildData();
+
+
+//ANIMATION LOOP:
+var tLast = 0,
+    dt = 0;
+d3.timer(function(t) {
+    dt = t - tLast;
+    tLast = t;
+
+    sliderDamp.redraw();
+    stepResponse.redraw();
+    GT_response.redraw();
+});
---
+++ test_code_skb.js
@@ -0,0 +1,94 @@
+/**
+ * Created by secretariat21 on 9/3/15. From code from https://groups.google.com/forum/#!topic/d3-js/vo-6AciRswU
+ * This code plots the Gibbs Free Energy function
+ */
+
+var width = 250;
+var height = 120;
+
+var xmin = 300E-9; // defining the x-axis: function undefined at 0
+var xmax = 1;
+var sample = 1000;
+
+var R = 8.3145; // J/mol*K
+var T = 273;    // K
+
+function gibbs_of_mixing(x){    // this is the function you want plotted
+    return R*T*(x*Math.log(x) + (1-x)*Math.log(1-x));
+}
+
+var x1 = d3.scale.linear().domain([0, sample]).range([xmin, xmax]); // maps the domain to the range (makes an array: x1(350) = 0.5)
+var data = d3.range(sample).map(function(d){ return {
+    x: x1(d),
+    intensity: gibbs_of_mixing(x1(d))};   // creates the data (x is x, intensity is y, for each point in xmin to xmax)
+});
+
+var x = d3.scale.linear().domain([xmin, xmax]).range([450, 450+width]); // puts the graph in the correct place on the screen
+var y = d3.scale.linear().domain(d3.extent(data.map(function(d){return d.intensity;}))).range([30+height, 30]);
+
+var line = d3.svg.line()    // turns the data into a line object
+    .x(function(d) { return x(d.x); })
+    .y(function(d) { return y(d.intensity); })
+
+d3.select("#panelTwo").append("path").attr("d", line(data))     // puts the line in the correct panel
+    .attr("class", "curve2");
+
+
+var width2 = 250;
+var height2 = 50;
+
+var Tmin = 0; // defining the x-axis: function undefined at 0
+var Tmax = 10;
+var sample = 1000;
+
+function Gibbs_phase(T){    // this is the function you want plotted
+    S = 10;
+    b = 10;
+    Gibbs_original = -S*T+b;
+    return Gibbs_original;
+}
+
+var x1 = d3.scale.linear().domain([0, sample]).range([Tmin, Tmax]);
+var data = d3.range(sample).map(function(d){ return {
+    x: x1(d),
+    G: Gibbs_phase(x1(d))};   // creates the data (x is x, intensity is y, for each point in xmin to xmax)
+});
+
+var x = d3.scale.linear().domain([Tmin, Tmax]).range([175, 175+width2]); // puts the graph in the correct place on the screen
+var y = d3.scale.linear().domain(d3.extent(data.map(function(d){return d.G;}))).range([70+height2, 70]);
+
+var line = d3.svg.line()    // turns the data into a line object
+    .x(function(d) { return x(d.x); })
+    .y(function(d) { return y(d.G); })
+
+d3.select("#panelPtMass").append("path").attr("d", line(data))     // puts the line in the correct panel
+    .attr("class", "curve2");
+
+var width3 = 275;
+var height3 = 120;
+
+var Tmin2 = 0; // defining the x-axis: function undefined at 0
+var Tmax2 = 10;
+
+function Gibbs_phase2(T){    // this is the function you want plotted
+    S = 25;
+    b = 10;
+    Gibbs_original = -S*T+b;
+    return Gibbs_original;
+}
+
+var x1 = d3.scale.linear().domain([0, sample]).range([Tmin2, Tmax2]);
+var data = d3.range(sample).map(function(d){ return {
+    x: x1(d),
+    G: Gibbs_phase2(x1(d))};   // creates the data (x is x, intensity is y, for each point in xmin to xmax)
+});
+
+var x = d3.scale.linear().domain([Tmin2, Tmax2]).range([300, 300+width3]); // puts the graph in the correct place on the screen
+var y = d3.scale.linear().domain(d3.extent(data.map(function(d){return d.G;}))).range([80+height3, 80]);
+
+var line = d3.svg.line()    // turns the data into a line object
+    .x(function(d) { return x(d.x); })
+    .y(function(d) { return y(d.G); })
+
+d3.select("#panelPtMass").append("path").attr("d", line(data))     // puts the line in the correct panel
+    .attr("class", "curve3");
